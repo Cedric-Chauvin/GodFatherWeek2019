06df922ed0e3f029886f67474966fc4e6f1c7b12
Bastien <bastien.barneoud@gmail>
2019-09-03 10:03:38 +0200

    Updated Movement,pickup and opening

---
+++ GodFatherWeed2019Unity/Assets/_Scripts/Object/ObjectPickUp.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectPickUp : MonoBehaviour
+{
+    public Text pickUpText;
+    public string Text;
+
+    private bool pickUpAllowed;
+
+    private void Start()
+    {
+        pickUpText.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+        {
+            PickUp();
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            pickUpText.text = Text;
+            pickUpText.gameObject.SetActive(true);
+            pickUpAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            pickUpText.gameObject.SetActive(false);
+            pickUpAllowed = false;
+        }
+    }
+
+    private void PickUp()
+    {
+        pickUpText.gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+}
--- GodFatherWeed2019Unity/Assets/_Scripts/Players/PlayerController.cs
+++ GodFatherWeed2019Unity/Assets/_Scripts/Players/PlayerController.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    #region Monobehaviour
+    Rigidbody rigidBody;
+    Camera viewCamera;
+    Vector3 velocity;
 
-    private void Update()
+    public float moveSpeed = 10.0f;
+    // Use this for initialization
+
+    void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
+        //	Cursor.visible = false;
     }
 
-    #endregion
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
+        //transform.LookAt(mousePos + Vector3.up * transform.position.y);
+        float inputHorizontal = Input.GetAxisRaw("P1_Horizontal");
+        float inputVertical = Input.GetAxisRaw("P1_Vertical");
+        Vector3 newVelocity = new Vector3(inputVertical * moveSpeed, 0.0f, inputHorizontal * -moveSpeed);
+        rigidBody.velocity = newVelocity;
+    }
 }
