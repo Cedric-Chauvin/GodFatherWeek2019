0d43d7e4978c6208084a33383d65db89d57ff98b
Artlock <maxime.filipovich@gmail.com>
2019-09-04 18:15:18 +0200

    Bis

--- GodFatherWeed2019Unity/Assets/_Scripts/Objects/CanneAPeche.cs
+++ GodFatherWeed2019Unity/Assets/_Scripts/Objects/CanneAPeche.cs
@@ -9,7 +9,7 @@ public class CanneAPeche : ObjectBase
     public override void Lancer(float dir)
     {
         Transform instance = Instantiate(ligne);
-        instance.GetComponent<MunCanne>().Setup(myPLayer, new Vector3(Mathf.Cos(Mathf.Deg2Rad * (dir - 90)), 0, -Mathf.Sin(Mathf.Deg2Rad * (dir - 90))), damage, dommage, speed);
+        instance.GetComponent<MunCanne>().Setup(myPLayer, new Vector3(Mathf.Cos(Mathf.Deg2Rad * (dir - 90)), 0, -Mathf.Sin(Mathf.Deg2Rad * (dir - 90))), distanceMax, dommage, speed);
         Destroy(this.gameObject);
     }
 }
