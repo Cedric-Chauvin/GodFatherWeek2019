1816745da7a386713d0274347cd99b869f0cc9cb
Artlock <maxime.filipovich@gmail.com>
2019-09-05 11:25:22 +0200

    Main menu added

---
+++ GodFatherWeed2019Unity/Assets/Resources/Scripts/MenuManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public Animator animator;
+    public bool isCredit=false;
+
+    public void Start()
+    {
+        animator.SetBool("menustart", true);
+    }
+
+    public void OpenScreen()
+    {
+        if (isCredit)
+            animator.SetBool("creditstart", true);
+        else
+            animator.SetBool("introstart", true);
+        animator.SetBool("menuclose", false);
+    }
+
+    public void CloseCredits()
+    {
+        animator.SetBool("creditstart", false);
+        animator.SetBool("creditsclose", true);
+    }
+
+    public void isCreditorNot(bool value)
+    {
+        isCredit = value;
+    }
+    public void closeMenu()
+    {
+        animator.SetBool("menustart", false);
+        animator.SetBool("menuclose", true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ GodFatherWeed2019Unity/Assets/Resources/Scripts/rotate1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate1 : MonoBehaviour
+{
+    private float rotationX, rotationY;
+    private float rotationZ = 0F;
+    private const float TIME_ROTATE = 1f / 100000;
+    private Vector3 rotationEuler;
+    void Update()
+    {
+        rotationEuler += Vector3.back * 8 * Time.deltaTime; //increment 30 degrees every second
+        transform.rotation = Quaternion.Euler(rotationEuler);
+    }
+}
