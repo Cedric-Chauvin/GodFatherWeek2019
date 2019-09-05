3e37bc7803385f59bae7b0c7fe161a9a5108d233
Artlock <maxime.filipovich@gmail.com>
2019-09-05 13:03:57 +0200

    Renamed Unity project (Weed>Week)

---
+++ GodFatherWeek2019Unity/Assets/_Scripts/Managers/SoundManager.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [Serializable]
+    public class SoundEffect
+    {
+        public string Name;
+        public AudioClip File;
+    }
+
+    public enum DefaultTypes
+    {
+        Any,
+        UseItem,
+        Death,
+        Win,
+        Damage
+    }
+
+    #region Singleton
+
+    public static SoundManager Instance;
+
+    #endregion
+
+    #region Public Variables
+
+    public AudioSource Music;
+
+    public AudioSource SFX;
+
+    public List<SoundEffect> Clips;
+
+    public List<SoundEffect> Items;
+    public List<SoundEffect> Deaths;
+    public List<SoundEffect> Wins;
+    public List<SoundEffect> Damage;
+
+    public bool VolumeToggle
+    {
+        get { return _volumeToggle; }
+        private set
+        {
+            _volumeToggle = value;
+
+            PlayerPrefs.SetInt("VolumeToggle", (_volumeToggle ? 1 : 0));
+
+            if (VolumeToggle)
+            {
+                SFX.volume = 1f;
+                Music.volume = 1f;
+            }
+            else
+            {
+                SFX.volume = 0f;
+                Music.volume = 0f;
+            }
+        }
+    }
+
+    #endregion
+
+    #region Private Variables
+
+    [SerializeField]
+    private bool _volumeToggle;
+
+    #endregion
+
+    #region Monobehaviour
+
+    private void Awake()
+    {
+        // Last minute code!
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+
+        // Prevent duplication of SoundManager
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this);
+
+        Instance = this;
+
+        if (PlayerPrefs.HasKey("VolumeToggle"))
+        {
+            VolumeToggle = (PlayerPrefs.GetInt("VolumeToggle") == 1);
+        }
+        else
+        {
+            VolumeToggle = true;
+            PlayerPrefs.SetInt("VolumeToggle", 1);
+        }
+    }
+
+    private void Start()
+    {
+        Music.Play();
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void PlaySFX(string soundName, DefaultTypes type = DefaultTypes.Any, float volume = 1f)
+    {
+        AudioClip clip = GetSound(soundName, type);
+
+        if (clip) SFX.PlayOneShot(clip, volume);
+    }
+    public void PlayRandomSFX( DefaultTypes type, float volume = 1f)
+    {
+        AudioClip clip = GetRandomSound(type);
+
+        if (clip) SFX.PlayOneShot(clip, volume);
+    }
+
+    public void ToggleVolume()
+    {
+        VolumeToggle = !VolumeToggle;
+    }
+
+    public void ChangeVolumeMusic(float vol)
+    {
+        Music.volume = vol;
+    }
+
+    public void ChangeVolumeSFX(float vol)
+    {
+        SFX.volume = vol;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private AudioClip GetSound(string soundName, DefaultTypes type = DefaultTypes.Any)
+    {
+        List<SoundEffect> searchList = new List<SoundEffect>();
+        GetListByType(type, out searchList);
+
+        foreach (SoundEffect sfx in searchList)
+        {
+            if (sfx.Name.ToLower().Trim().Equals(soundName.ToLower().Trim()))
+            {
+                return sfx.File;
+            }
+        }
+
+        Debug.LogError("Sound not found - " + soundName);
+        return null;
+    }
+
+    private AudioClip GetRandomSound(DefaultTypes type)
+    {
+        List<SoundEffect> searchList = new List<SoundEffect>();
+        GetListByType(type, out searchList);
+
+        if (searchList.Count > 0)
+        {
+            System.Random rng = new System.Random();
+            int random = rng.Next(0, searchList.Count);
+
+            return searchList[random].File;
+        }
+
+        Debug.LogError("Random sound not found for type - " + type);
+        return null;
+    }
+
+    private void GetListByType(DefaultTypes type, out List<SoundEffect> searchList)
+    {
+        switch (type)
+        {
+            case DefaultTypes.UseItem:
+                searchList = Items;
+                break;
+            case DefaultTypes.Death:
+                searchList = Deaths;
+                break;
+            case DefaultTypes.Win:
+                searchList = Wins;
+                break;
+            case DefaultTypes.Damage:
+                searchList = Damage;
+                break;
+            default:
+                searchList = Clips;
+                break;
+        }
+    }
+
+    #endregion
+}
---
+++ GodFatherWeek2019Unity/Assets/_Scripts/Players/PlayerController.cs
@@ -0,0 +1,250 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public static List<PlayerController> _players = new List<PlayerController>();
+
+    private Rigidbody rigidBody;
+    private Camera viewCamera;
+    public Animator animator;
+
+    public ChestOpening chest_opening;
+
+    [field: Header("Number of the player, corresponds to the controller"), SerializeField]
+    public int playerNumber { get; private set; } = 1;
+
+    [Header("Input related settings")]
+    public bool keyboard = false;
+
+    [Header("Movement related settings")]
+    public float deadzone = 0.1f;
+    [Range(1f, 10f)]
+    public float moveSpeed = 4f;
+
+    [Header("Items")]
+    private ObjectBase itemInRange;
+    private ObjectBase currentItem;
+
+    [Header("Cooldowns")]
+    [Range(1f, 20f)]
+    public float pickupCooldown = 4f;
+    private float lastPickupTime;
+
+    [Range(0.1f, 2f)]
+    public float damageCooldown = 0.5f;
+    private float lastDamageTime;
+
+    [Range(0.5f, 2f)]
+    public float beforeUseCooldown = 1f;
+
+    [field: Header("Health")]
+    public float health { get; private set; } = 100f;
+    private float maxHealth; 
+    private bool dead = false;
+    private bool stun = false;
+
+    [Header("UI")]
+    public Text Ammo;
+    public Image HP;
+
+    private void OnDestroy()
+    {
+        _players.Remove(this);
+    }
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
+        animator = GetComponentInChildren<Animator>();
+
+        lastPickupTime -= pickupCooldown;
+        lastDamageTime -= damageCooldown;
+    }
+
+    private void Start()
+    {
+        _players.Add(this);
+        Ammo.text = "0";
+        maxHealth = health;
+    }
+
+    private void Update()
+    {
+        if (currentItem != null)
+        {
+            Ammo.text = currentItem.nbUtilisation.ToString();
+            transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else
+        {
+            Ammo.text = "0";
+            transform.GetChild(1).gameObject.SetActive(false);
+        }
+
+        if (dead) return;
+
+        if (stun) return;
+
+        MovementAndOrientation();
+
+        Interactions();
+    }
+
+    private void MovementAndOrientation()
+    {
+        // Player movement and rotation
+
+        float inputHorizontal;
+        float inputVertical;
+        Vector3 aim;
+
+        if (!keyboard) // TWO JOYSTICKS
+        {
+            inputHorizontal = Input.GetAxis("P" + playerNumber + "_Horizontal");
+            inputVertical = Input.GetAxis("P" + playerNumber + "_Vertical");
+
+            float inputRightHorizontal = Input.GetAxis("P" + playerNumber + "_Horizontal_Right");
+            float inputRightVertical = Input.GetAxis("P" + playerNumber + "_Vertical_Right");
+
+            aim = new Vector3(inputRightHorizontal, 0f, inputRightVertical);
+
+            // Applying rotation
+            if (aim.magnitude > deadzone)
+                transform.rotation = Quaternion.LookRotation(aim);
+        }
+        else // KEYBOARD
+        {
+            // Player rotation towards mouse
+
+            Vector3 cursorScreenPoint = Input.mousePosition;
+            Vector3 playerScreenPoint = viewCamera.WorldToScreenPoint(transform.position);
+
+            // Z axis removed as it's the distance to the camera
+            playerScreenPoint = new Vector3(playerScreenPoint.x, playerScreenPoint.y, 0f);
+
+            aim = (cursorScreenPoint - playerScreenPoint).normalized;
+
+            // Screen XY is world XZ
+            aim = new Vector3(aim.x, 0f, aim.y);
+
+            // Applying rotation
+            transform.rotation = Quaternion.LookRotation(aim);
+
+            // Player movement
+            inputHorizontal = Input.GetAxis("KB_Horizontal");
+            inputVertical = Input.GetAxis("KB_Vertical");
+        }
+
+        // Applying movement;
+        rigidBody.velocity = new Vector3(Mathf.Clamp(inputHorizontal, -1, 1) * moveSpeed, rigidBody.velocity.y, Mathf.Clamp(inputVertical, -1, 1) * moveSpeed);
+
+        // Make current item follow player
+        if (currentItem)
+        {
+            currentItem.transform.position = transform.position;
+            currentItem.transform.rotation = transform.rotation;
+        }
+        
+        animator.SetFloat("Speed", rigidBody.velocity.sqrMagnitude); // RUNNING ANIMATION
+    }
+
+    private void Interactions()
+    {
+
+        if (currentItem && Input.GetAxis("P" + playerNumber + "_Action_Axis") == 1f && Time.time > (lastPickupTime + beforeUseCooldown))
+        {
+            if (currentItem.Utilisation(transform.rotation.eulerAngles.y, this))
+                currentItem = null;
+        }
+        else if (itemInRange && Time.time > (lastPickupTime + pickupCooldown) && Input.GetAxis("P" + playerNumber + "_Action_Axis") == 1f)
+        {
+            lastPickupTime = Time.time;
+            currentItem = itemInRange;
+
+            // Remove picked up item from in range, from all players
+            foreach (PlayerController pc in _players)
+                if (pc.GetItemInRange() == currentItem) pc.SetItemInRange(null);          
+
+            // Remove pickup script from current item
+            Destroy(currentItem.gameObject.GetComponent<ObjectPickUp>());
+
+            SoundManager.Instance.PlaySFX("Pickup", SoundManager.DefaultTypes.UseItem);
+            animator.SetBool("InRange", true); // PICKUP ANIMATION
+            animator.SetTrigger("UseRT"); // PICKUP ANIMATION
+
+            animator.SetBool("InRange", false); // RESET MULTI CONDITION
+        }
+    }
+
+    public ObjectBase GetItemInRange()
+    {
+        return itemInRange;
+    }
+
+    public void SetItemInRange(ObjectBase obj)
+    {
+        itemInRange = obj;
+    }
+
+    public void Damage(float dmg)
+    {
+        if (health > 0f && Time.time > (lastDamageTime + damageCooldown))
+        {
+            lastDamageTime = Time.time;
+            health -= dmg;
+
+            if (health <= 0)
+                health = 0;
+
+            HP.fillAmount = health / maxHealth;
+
+            Debug.Log(health);
+
+            if (playerNumber == 1)
+                SoundManager.Instance.PlaySFX("Hurt_male", SoundManager.DefaultTypes.Damage);
+            else
+                SoundManager.Instance.PlaySFX("Hurt_female", SoundManager.DefaultTypes.Damage);
+            animator.SetTrigger("Hit"); // HIT ANIMATION
+
+            ChestOpening.Instance.PotentialRestart(transform);
+
+            if (health <= 0f) Die();
+        }
+        else
+            Debug.Log("Dead already!");
+    }
+
+    public void Die()
+    {
+        if (playerNumber == 1)
+            SoundManager.Instance.PlaySFX("Death_male", SoundManager.DefaultTypes.Death);
+        else
+            SoundManager.Instance.PlaySFX("Death_female", SoundManager.DefaultTypes.Death);
+        animator.SetBool("Alive", false); // Death animation
+        dead = true;
+        foreach (var pc in _players)
+        {
+            if (pc != this) chest_opening.Victory(pc);
+        }
+        
+        
+    }
+
+    public void StunPlayer(float time)
+    {
+        stun = true;
+        animator.SetTrigger("Stun");
+        ChestOpening.Instance.PotentialRestart(transform);
+        Invoke("DeStunPlayer", time);
+    }
+
+    public void DeStunPlayer()
+    {
+        ChestOpening.Instance.PotentialRestart(transform);
+        stun = false;
+    }
+}
